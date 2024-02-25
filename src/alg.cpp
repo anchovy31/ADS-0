b681a0791613a97ae55927b4c2b0c633df15e3a2
anchovy31 <146199120+anchovy31@users.noreply.github.com>
2024-02-25 14:30:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while(a!=0 and b!=0)
-    {
-        if (a>b) a=a-b;
-        else b=b-a;
-    }
-    if (a!=0) return a;
-    else return b;
+	while (a != 0 && b != 0)
+	{
+		if (a > b) a = a - b;
+		else b = b - a;
+	}
+	if (a != 0) return a;
+	else return b;
 }
