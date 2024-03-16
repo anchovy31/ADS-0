c140d76be316344e4eaa3635b1733c5349de8599
anchovy31 <146199120+anchovy31@users.noreply.github.com>
2024-03-16 19:49:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,10 +4,12 @@ int gcd(int a, int b) {
         // поместить сюда текст реализации алгоритма (на С++)
   int b = 0, c = 5;
   while (a != 0 && b != 0) {
-    if (a > b)
+    if (a > b) {
      a = a - b;
-    else if (a <= b)
+    }
+    else if (a <= b) {
      b = b - a;
+    }
   }
     if (a != 0)
      return a;
